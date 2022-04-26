0a754861dafb10c2ceaae0c31811cb87cabd50f5
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-04-26 18:27:55 +0200

    aLpHaBeT

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
-i = 122
-temp = 32
-while i >= 65:
-    print("{:c}".format(i), end="")
-    if i == 65:
-        break
-    i -= 1
-    i -= temp
-    temp *= -1
+
+for letter in range(122, 96, -1):
+    if letter % 2 == 0:
+        print("{:c}".format(letter), end="")
+    else:
+        letter -= 32
+        print("{:c}".format(letter), end="")
