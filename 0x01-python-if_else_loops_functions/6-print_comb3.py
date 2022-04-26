2c7a07ad19af390adacc6f4317178a05656a57e0
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-04-26 13:12:38 +0200

    Write a program that prints all possible different combinations of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+a = 0
+b = 1
+
+while a < 8:
+    print("{}{}".format(a, b), end=", ")
+    if b == 9:
+        a += 1
+        b = a + 1
+    else:
+        b += 1
+print(89)
