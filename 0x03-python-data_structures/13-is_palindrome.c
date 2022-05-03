df03de30f05eafef9203b348c68a1bdc15fb3f40
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-05-03 11:35:05 +0200

    Linked list palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp = *head;
+	int idx = 0, i = 0, j, check = 0;
+	int array[2048];
+
+	while (tmp)
+	{
+
+		array[idx] = tmp->n;
+		tmp = tmp->next;
+		idx++;
+	}
+	idx = idx - 1;
+	j = idx;
+	while (i <= (idx / 2) && j >= (idx / 2))
+	{
+		if (array[i] != array[j])
+		{
+			check = 1;
+			break;
+		}
+		i++;
+		j--;
+	}
+	if (check == 1)
+		return (0);
+	else
+		return (1);
+}
