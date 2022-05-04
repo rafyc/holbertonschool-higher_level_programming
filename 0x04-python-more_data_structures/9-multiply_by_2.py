d40c81e7268fe53c2b2fa15ee27a67e6e2ad1b2a
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-05-04 14:49:55 +0200

    multiply by 2 all values

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,6 @@
 
 def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
-    for i in new_dict:
-        new_dict[i] *= 2
+    for key in new_dict:
+        new_dict[key] *= 2
     return new_dict
