2a97a23aa4e4401e52a789946d60292ed2f28cef
Raphaël Chemouni <45367323+rafyc@users.noreply.github.com>
2022-05-06 10:12:43 +0200

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,10 @@
 
 def best_score(a_dictionary):
     if a_dictionary:
-        v = list(a_dictionary.values())
-        k = list(a_dictionary.keys())
-        return k[v.index(max(v))]
+        valueurs = list(a_dictionary.values())
+        cles = list(a_dictionary.keys())
+        return cles[valeurs.index(max(valeurs))]
     else:
         return None
+
+    """The index() method returns the index of the specified element in the list."""
