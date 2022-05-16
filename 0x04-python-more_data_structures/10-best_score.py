f2438e3ebd129d346525020ab74dac2cd8e5117c
Raphaël Chemouni <45367323+rafyc@users.noreply.github.com>
2022-05-16 14:10:23 +0200

    new method

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -9,3 +9,14 @@ def best_score(a_dictionary):
         return None
 
 #The index() method returns the index of the specified element in the list.
+
+   """ 
+   if a_dictionary:
+        score = 0
+        for key in a_dictionary:
+            if a_dictionary[key] > score:
+                score = a_dictionary[key]
+                best = key
+        return best
+    return None
+    """
