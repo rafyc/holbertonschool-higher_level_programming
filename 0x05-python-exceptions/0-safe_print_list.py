ed045ccac41628e8d788b86db1ec777f012d1c70
rafyc <rapha.chemouni@gmail.com>
2022-05-16 06:30:56 -0400

    first push + Readme

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+
+    try:
+        for elements in my_list:
+            if count < x:
+                print("{}".format(my_list[count]), end="")
+                count += 1
+        print("")
+    except IndexError:
+        pass
+    return count
