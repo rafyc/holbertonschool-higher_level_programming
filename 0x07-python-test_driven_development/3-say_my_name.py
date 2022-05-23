9c4870e9006742384bd07462ce5e554b52da3472
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-05-23 17:56:44 +0200

    new test files

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -9,9 +9,9 @@ def say_my_name(first_name, last_name=""):
         first_name (str): the frist name
         last_name (str, optional): the last name. Defaults to "".
     """
-    if type(first_name) is not str:
+    if isinstance(first_name, str) is False:
         raise TypeError("first_name must be a string")
-    if type(last_name) is not str:
+    elif isinstance(last_name, str) is False:
         raise TypeError("last_name must be a string")
     else:
         print(f"My name is {first_name} {last_name}")
