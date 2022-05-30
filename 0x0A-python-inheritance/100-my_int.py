7464edd26fcf3110810fa8ca200cc9c8e2471f41
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-05-30 18:53:54 +0200

    fix comment

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
+"""Create a class MyInt that inherits from int
+"""
+
 
 class MyInt(int):
+    """
+    The class MyInt that inherits from int:
+    """
 
     def __init__(self, b):
+        """The initialize method"""
         self.__b = b
 
     def __eq__(self, b):
+        """The equal method return non-equal value"""
         return self.__b != self.__b
 
     def __ne__(self, b):
+        """The non-equal method return equal value"""
         return self.__b == self.__b
