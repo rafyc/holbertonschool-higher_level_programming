a690d2e101591c2ef830f47b535777d2e7af2e38
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-05-30 18:54:54 +0200

    fix comment

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""
+The module define the function 'add_attribute()'
+"""
 
 
 def add_attribute(a, key, value):
+    """
+    The function adds a new attribute to an object if it's possible.
+    Args:
+        - obj: the name of the object instance
+        - key (str): the attribute name
+        - value: the value to set the name attribute to
+    """
     if hasattr(a, '__dict__') is False:
         raise TypeError("can't add new attribute")
     else:
