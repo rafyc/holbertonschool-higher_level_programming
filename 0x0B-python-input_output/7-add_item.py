b23272e2d49b383ade82e79f31d6e6aa68b9255d
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-05-31 17:21:19 +0200

    fix comments

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,9 +2,10 @@
 
 
 from sys import argv
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
 
 file = "add_item.json"
 try:
