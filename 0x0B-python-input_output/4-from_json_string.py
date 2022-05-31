c85cdf1c4170041910f75a1293a3ca61399e1f16
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-05-31 17:13:11 +0200

    fix comments

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -8,4 +8,10 @@ import json
 
 
 def from_json_string(my_str):
+    """ function that returns an object
+    (Python data structure) represented by a JSON string:
+
+    Args:
+        my_str: a string
+    """
     return json.loads(my_str)
