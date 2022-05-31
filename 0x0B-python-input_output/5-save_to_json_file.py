bd6b9d1a57788fb3f6953d794663a32228cdabee
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-05-31 17:16:35 +0200

    fix comments

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,5 +7,10 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """    with open(filename, 'w', encoding="utf_8") as myfile:
+        return myfile.write(json.dumps(my_obj))
+    Returns:
+        _type_: _description_
+    """
     with open(filename, 'w', encoding="utf_8") as myfile:
         return myfile.write(json.dumps(my_obj))
