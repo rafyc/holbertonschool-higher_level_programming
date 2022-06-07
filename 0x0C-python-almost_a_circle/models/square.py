cda6e73a707a95fc5eeddca6a4e5affdbd79eb31
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-06-07 13:10:16 +0200

    fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,7 +13,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ __init__ method to create an object instance of the class Rectangle
         """
-
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
