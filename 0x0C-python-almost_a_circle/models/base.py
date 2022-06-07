306d02d58654221532ca2aa49849973dd0ea3471
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-06-07 14:07:12 +0200

    fix return str instead of list

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,7 +30,7 @@ class Base:
         if list_dictionaries is not None:
             return json.dumps(list_dictionaries)
         else:
-            return []
+            return "[]"
 
     @classmethod
     def save_to_file(cls, list_objs):
