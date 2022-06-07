d69aeaebc3a3d9b61b69b6a9ae756de28d410f0c
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-06-07 10:28:09 +0200

    first commit

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
-import unittest
+
 from models.rectangle import Rectangle
+from models.base import Base
+from models.square import Square
+import unittest
 
 
 class TestRect(unittest.TestCase):
