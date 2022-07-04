29967f53d8840caee7e55d95c4c86b36ceb0c72f
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-07-04 08:15:26 +0200

    corrected syntax

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Write a script that lists all the cities of California that
 -- can be found in the database hbtn_0d_usa
 
-SELECT id,  FROM hbtn_0d_usa WHERE
+SELECT id, name FROM cities ORDER BY id ASC;
