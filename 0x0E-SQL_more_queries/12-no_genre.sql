6bce814029850eefaa32b02997e5aeb2846c80e6
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-07-06 14:05:50 +0200

    Write a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- Write a script that lists all shows contained in hbtn_0d_tvshows without
+-- a genre linked.
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
