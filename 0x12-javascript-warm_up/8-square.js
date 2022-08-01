efbdcef4186880f2df1446dea8e96a617f8bf25d
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-01 12:54:02 +0200

    fix condition + else

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,8 @@ const num = parseInt(process.argv[2]);
 
 if (process.argv[2] === undefined) {
   console.log('Missing size');
-}
-for (let i = 0; i < num; i++) {
-  console.log('X'.repeat(num));
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
 }
