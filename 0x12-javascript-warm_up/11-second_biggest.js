85a493a905692653c6b6a5c423c317174fc551da
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-03 10:00:50 +0200

    fix sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 if (process.argv.length < 4) {
   console.log(0);
 } else {
   const myNum = [];
   for (let i = 2; i < process.argv.length; i++) {
-    myNum.push(parseInt(process.argv[i]));
+    if (!isNaN(process.argv[i])) myNum.push(process.argv[i]);
   }
 
   myNum.sort((a, b) => a - b);
