4ad5717af466f25dcd8694c778cb8a8199083127
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-04 11:28:41 +0200

    README

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,5 +3,5 @@
 import MySQLdb
 
 db = MySQLdb.connect(
-  host='localhost', port='3306', passwd='',user='root', db='hbtn_0e_0_usa')
+  host='localhost', port=3306, passwd='',user='root', db='hbtn_0e_0_usa')
 cur = db.cursor("SHOW TABLES")
