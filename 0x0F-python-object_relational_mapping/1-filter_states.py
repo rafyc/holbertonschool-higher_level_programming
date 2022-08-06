91798c0af4193e02709bc1a72349003a50c1e684
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-06 12:44:20 +0200

    first push

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa"""
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    db = MySQLdb.connect(
-      host='localhost',
-      port=3306,
-      user=argv[1],
-      passwd=argv[2],
-      db=argv[3],
-      charset="utf8"
-      )
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id")
-
-    rows = cur.fetchall()
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                                 passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cursor.fetchall()
     for row in rows:
         if row[1][0] == 'N':
             print(row)
-    cur.close()
+    cursor.close()
     db.close()
