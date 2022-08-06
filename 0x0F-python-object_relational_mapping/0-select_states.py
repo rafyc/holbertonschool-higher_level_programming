1393aef60514d8c81adaede7f8eb6f39c6be215b
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-06 12:25:14 +0200

    first push

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
       port=3306,
       user=argv[1],
       passwd=argv[2],
-      db=argv[3]
+      db=argv[3],
+      charset="utf8"
       )
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id")
