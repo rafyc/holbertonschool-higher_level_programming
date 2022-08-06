97730cb6fc6540cd8e290d88743395eda7c9750c
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-06 12:38:51 +0200

    first push

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,19 +5,19 @@ from sys import argv
 
 if __name__ == '__main__':
 
-  db = MySQLdb.connect(
-    host='localhost',
-    port=3306,
-    user= argv[1],
-    passwd= argv[2],
-    db= argv[3]
-    )
-  cur = db.cursor()
-  cur.execute("SELECT * FROM states ORDER BY id")
+    db = MySQLdb.connect(
+      host='localhost',
+      port=3306,
+      user=argv[1],
+      passwd=argv[2],
+      db=argv[3]
+      )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
 
-  rows = cur.fetchall()
-  for row in rows:
-      if row[1][0] == 'N':
-         print(row)
-  cur.close()
-  db.close()
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
+    cur.close()
+    db.close()
