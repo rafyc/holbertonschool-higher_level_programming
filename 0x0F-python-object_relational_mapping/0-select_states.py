eb911c4c43dc4c8a82234abb1c9cbecdf92c1936
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-06 12:19:00 +0200

    first push

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-
-import MySQLdb
-from sys import argv
+"""Script that lists all states from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
+  import MySQLdb
+  from sys import argv
+
+
 
   db = MySQLdb.connect(
     host='localhost',
