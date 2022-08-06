0f0f6db653c21b80dde9906396b2260a7f9acc64
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-06 12:55:57 +0200

    first push

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-
-import MySQLdb
-from sys import argv
+"""Script that takes in an argument and displays all values in the states
+ table of hbtn_0e_0_usa where name matches the argument"""
 
 if __name__ == '__main__':
+
+    import MySQLdb
+    from sys import argv
 
     db = MySQLdb.connect(
       host='localhost',
