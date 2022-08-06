57b59632fdf1f8d19f6bb7eb6f6eed734adcc67d
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-06 12:28:44 +0200

    first push

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-"""Script that lists all states from the database hbtn_0e_0_usa"""
+"""
+lists all states from the database hbtn_0e_0_usa
+Arguments:
+    mysql username sys.argv[1]
+    mysql password sys.argv[2]
+    database name sys.argv[3]
+"""
+
+import sys
+import MySQLdb
 
 if __name__ == '__main__':
-    import MySQLdb
-    from sys import argv
-
     db = MySQLdb.connect(
-      host='localhost',
-      port=3306,
-      user=argv[1],
-      passwd=argv[2],
-      db=argv[3],
-      charset="utf8"
-      )
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id")
 
