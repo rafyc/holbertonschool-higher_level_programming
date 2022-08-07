3695abc2dc9201a00d3f537efdc670fe85114c1e
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-07 11:15:46 +0200

    first push

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,7 +7,12 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == '__main__':
-    engine = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}', pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1],
+        argv[2],
+        argv[3],
+        pool_pre_ping=True
+        ))
     Base.metadata.create_all(engine)
 
     session = Session(engine)
