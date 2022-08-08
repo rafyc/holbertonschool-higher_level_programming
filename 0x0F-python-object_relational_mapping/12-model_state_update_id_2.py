03d2e61707324ea7436d99876d8445e7877042fe
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-08 11:54:50 +0200

    first push

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sys import argv
+from model_state import Base, State
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1],
+        argv[2],
+        argv[3],
+        pool_pre_ping=True
+        ))
+    session = Session(engine)
+    Base.metadata.create_all(engine)
+    myvar = session.query(State).filter(State.id == 2).first()
+    myvar.name = "New Mexico"
+    session.commit()
+    session.close()
