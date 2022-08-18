83c80f6416b8997a4962cbd57626c864e1f8b140
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-18 15:00:04 +0200

    first push

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ from sys import argv
 
 if __name__ == '__main__':
     data = parse.urlencode({"email": argv[2]}).encode('ascii')
-    req =  request.Request(argv[1], data=data)
+    req =  request.Request(argv[1], data)
     with urlopen(req) as response:
         html = response.read().decode('utf-8')
         print(html)
