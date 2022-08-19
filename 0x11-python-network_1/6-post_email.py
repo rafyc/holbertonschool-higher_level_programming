4fde0aface817cfd68376f91554310d15eb90cf4
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-19 11:11:41 +0200

    first push

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
     url = argv[1]
     myobj = {'email': argv[2]}
 
-    x = requests.post(url, my_email=myobj)
+    x = requests.post(url, json=myobj)
 
     print(x.text)
