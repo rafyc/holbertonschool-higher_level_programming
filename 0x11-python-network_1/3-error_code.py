abc7845a1dd4f380945714d114c33ac151272169
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-19 11:03:06 +0200

    first push

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
         with urlopen(argv[1]) as response:
             print(response.read().decode("utf-8"))
     except HTTPError as e:
-        status_code = e.response.status_code
-        print(f"Error code: {status_code}")
+        status_code = e.response.code
+        print("Error code: {}".format(status_code))
