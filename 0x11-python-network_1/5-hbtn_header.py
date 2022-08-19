2c76a1419e3009df8fa2a1595bfbc0d0ddd5f330
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-19 12:43:11 +0200

    fix fstring

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Write a Python script that takes in a URL, sends a request to the URL
 and displays the value of the variable X-Request-Id in the response header
 """
