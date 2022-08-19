127b5d0cf5787a9befd9622bae5f357d6099a39d
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-19 11:09:38 +0200

    first push

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter, and displays
+the body of the response (decoded in utf-8)
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    myobj = {'email': argv[2]}
+
+    x = requests.post(url, email=myobj)
+
+    print(x.text)
