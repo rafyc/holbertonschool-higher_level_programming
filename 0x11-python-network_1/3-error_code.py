ef27062d4763f99cf95851156ff1d4855d4e376d
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-19 10:22:58 +0200

    first push

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,6 +5,10 @@ the body of the response (decoded in utf-8)."""
 if __name__ == '__main__':
     from urllib.request import urlopen
     from sys import argv
-
-    with urlopen(argv[1]) as response:
-        print(response.read())
+    from urllib.error import HTTPError
+    try:
+        with urlopen(argv[1]) as response:
+            print(response.read())
+    except HTTPError as e:
+        status_code = e.response.status_code
+        print(f"Error code: {status_code}")
