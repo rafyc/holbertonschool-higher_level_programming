832557117294727a418ed3ecba9ceb84c62adce6
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-09-13 12:37:38 +0200

    fix str code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,8 +4,8 @@ const axios = require('axios');
 
 axios.get(process.argv[2])
   .then(res => {
-    console.log('code: ', res.status);
+    console.log('code: ' + res.status);
   })
   .catch(err => {
-    console.log('code: ', err.response.status);
+    console.log('code: ' + err.response.status);
   });
